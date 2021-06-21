ef84690703e5c40ca32bf92105f445d44697be19
jeeseungbae <https://github.com/jeeseungbae/TIMD_project.git>
2021-06-21 15:12:17 +0900

    feat : create Input Class - String 입력

---
+++ src/main/java/stringCalculator/Main.java
@@ -0,0 +1,12 @@
+package stringCalculator;
+
+import java.io.IOException;
+
+public class Main{
+
+    public static void main(String[] args) throws IOException {
+        InputOuputSystem inputOuputSystem = new InputOuputSystem();
+        while(inputOuputSystem.solve());
+    }
+
+}
