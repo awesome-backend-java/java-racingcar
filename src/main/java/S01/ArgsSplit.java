fc1a8e278709595f63f660e08cbbd50542462ef2
jeeseungbae <https://github.com/jeeseungbae/TIMD_project.git>
2021-06-23 12:53:46 +0900

    refector : String 문자열 이어붙이기 StringBuilder로 type 변경

--- src/main/java/S01/ArgsSplit.java
+++ src/main/java/S01/ArgsSplit.java
@@ -2,23 +2,26 @@ package S01;
 
 public class ArgsSplit {
 
-    public String[] expression;
+    private String[] expression;
+    private final StringBuilder sb;
 
+    public ArgsSplit(){
+        sb = new StringBuilder();
+    }
     // 구별된 부호와 숫자 리턴
     public String[] divide(String args) {
         expression = findWord(args);
         return expression;
     }
-
     // String[] 으로 부호와 숫자 구별
     private String[] findWord(String args) {
-        String s = "";
+        sb.delete(0,sb.length());
         for (int i = 0; i < args.length(); i++) {
-            s += compare(args.charAt(i));
+            sb.append(compare(args.charAt(i)));
         }
-        return s.split(" ");
+        return sb.toString()
+                .split(" ");
     }
-
     // 숫자면 붙여서 부호면 띄어쓰기, +,-,*,/ 이외의 문자 무시
     private String compare(char ch) {
         if ('0' <= ch && ch <= '9' || ch == '.') {
