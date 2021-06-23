7fe33b34dee35ab881614c388d55949c59ccd8bc
jeeseungbae <https://github.com/jeeseungbae/TIMD_project.git>
2021-06-23 17:41:17 +0900

    refector = HashMap 선언에서 interface Map으로 선언 후 HashMap 주입

--- src/main/java/S01/Calculator.java
+++ src/main/java/S01/Calculator.java
@@ -1,22 +1,23 @@
 package S01;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.function.BiFunction;
 
 public class Calculator {
 
-    private final HashMap<String, BiFunction<Double, Double, Double>> calculatorHashMap;
+    private final Map<String, BiFunction<Double, Double, Double>> calculatorMap;
 
     public Calculator() {
-        calculatorHashMap = new HashMap<>();
-        calculatorHashMap.put("+", (num1, num2) -> num1 + num2);
-        calculatorHashMap.put("-", (num1, num2) -> num1 - num2);
-        calculatorHashMap.put("*", (num1, num2) -> num1 * num2);
-        calculatorHashMap.put("/", (num1, num2) -> num1 / num2);
+        calculatorMap = new HashMap<>();
+        calculatorMap.put("+", (num1, num2) -> num1 + num2);
+        calculatorMap.put("-", (num1, num2) -> num1 - num2);
+        calculatorMap.put("*", (num1, num2) -> num1 * num2);
+        calculatorMap.put("/", (num1, num2) -> num1 / num2);
     }
 
     // 부호에 따른 계산
     public double compute(double x, String op, double y) {
-        return calculatorHashMap.get(op).apply(x, y);
+        return calculatorMap.get(op).apply(x, y);
     }
 }
