7fd1fb71472826017bbbfbd3d7e7e5b1aeca164a
jeeseungbae <https://github.com/jeeseungbae/TIMD_project.git>
2021-06-25 16:34:30 +0900

    refector : convert method naming 및 logic 변경 , convertDecimal 추가

--- src/main/java/S01/CalculatorOptional.java
+++ src/main/java/S01/CalculatorOptional.java
@@ -1,9 +1,6 @@
 package S01;
 
 public class CalculatorOptional {
-    private String[] expression;
-    private double answer;
-    private final double standardDecimal = 0.1;
     private final SentenceSplit argsSplit;
     private final Calculator calculator;
 
@@ -15,27 +12,40 @@ public class CalculatorOptional {
 
     // 입력된정보 숫자와,부호 나눠서 앞에서부터 세개씩 계산
     public String computeAll(String args) {
-        expression = argsSplit.divide(args); // 숫자와 부호 String[] 으로 나누기
-        answer = convert(expression[0]);    // 처음 숫자 정보 입력
+        String[] expression = argsSplit.divide(args); // 숫자와 부호 String[] 으로 나누기
+        double total = divide(expression);
+        return String.valueOf(total);
+    }
+
+    // 입력된 정보 3개 단위로 분리하며 계산
+    private double divide(String[] expression) {
+        double answer = convertDouble(expression[0]);    // 처음 숫자 정보 입력
         for (int i = 1; i < expression.length; i += 2) {
-            answer = compute(answer, expression[i], convert(expression[i + 1]));
+            answer = compute(answer, expression[i], convertDouble(expression[i + 1]));
         }
-        return String.valueOf(answer);
+        return answer;
     }
 
     // 부호에 따른 계산
-    private double compute(double x, String op, double y) {
-        return calculator.compute(x, op, y);
+    private double compute(double frontNumber, String operation, double backNumber) {
+        return calculator.compute(frontNumber, operation, backNumber);
     }
 
     // double형 소수점 변환
-    private double convert(String s) {
-        int natural = s.indexOf('.');
-        int decimalCount = s.length() - natural - 1;
-        if (natural < 0) {
-            return Double.parseDouble(s);
+    private double convertDouble(String sentence) {
+        int naturalNumber = sentence.indexOf('.');
+        if (naturalNumber < 0) {
+            return Double.parseDouble(sentence);
         }
-        String expression = s.replace(".", "");
+        return convertDecimal(sentence, naturalNumber);
+    }
+
+    private double convertDecimal(String sentence, int naturalNumber) {
+        int decimalCount = sentence.length() - naturalNumber - 1;
+        String expression = sentence.replace(".", "");
+        double standardDecimal = 0.1;
+
         return Long.parseLong(expression) * Math.pow(standardDecimal, decimalCount);
     }
+
 }
