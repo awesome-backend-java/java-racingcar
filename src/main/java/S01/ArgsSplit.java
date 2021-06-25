5a059b79002cb14b9bb62a206a4cbea89fc79e03
jeeseungbae <https://github.com/jeeseungbae/TIMD_project.git>
2021-06-25 14:09:47 +0900

    style : compareByWord 메서드 주석 수정, naming 변경

--- src/main/java/S01/ArgsSplit.java
+++ src/main/java/S01/ArgsSplit.java
@@ -25,18 +25,19 @@ public class ArgsSplit {
     private String[] findWord(String args) {
         wordSplit.delete(0, wordSplit.length());
         for (int i = 0; i < args.length(); i++) {
-            wordSplit.append(compare(args.charAt(i)));
+            String word = compareByWord(args.charAt(i));
+            wordSplit.append(word);
         }
         return wordSplit.toString()
                 .split(" ");
     }
-    // 숫자면 붙여서 부호면 띄어쓰기, +,-,*,/ 이외의 문자 무시
-    private String compare(char word) {
+    // 숫자와 부호를 구별
+    private String compareByWord(char word) {
         if (zero <= word && word <= nine || word == decimal) {
-            return String.valueOf(word);
+            return String.valueOf(word); // 숫자면 계속 문자를 이어붙인다.
         }
         if (word == plus || word == minus || word == mul || word == div) {
-            return " " + word + " ";
+            return " " + word + " "; // 부호라면 띄어쓰기로 분리한다.
         }
         return "";
     }
