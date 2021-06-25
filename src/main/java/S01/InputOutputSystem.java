054ee6643101f2dffd28710b6a726411f78d2adf
jeeseungbae <https://github.com/jeeseungbae/TIMD_project.git>
2021-06-25 15:25:37 +0900

    style : 포맷팅

--- src/main/java/S01/InputOutputSystem.java
+++ src/main/java/S01/InputOutputSystem.java
@@ -2,7 +2,6 @@ package S01;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class InputOutputSystem {
@@ -17,8 +16,9 @@ public class InputOutputSystem {
         calculatorOptional = new CalculatorOptional();
         bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         endCalculator = "0";
-        expression="";
+        expression = "";
     }
+
     //입력정보를 받고 받은 정보 CalculatorOptional 클래스로 전달
     public void input() throws IOException {
         expression = print().readLine();
@@ -29,17 +29,21 @@ public class InputOutputSystem {
             System.out.println("-------- 잘못된 입력입니다. 다시 입력해주세요 --------");
         }
     }
+
     // 입력한 정보가 0일때 종료
     public boolean systemStatus() {
         return !expression.equals(endCalculator);
     }
-    private BufferedReader print(){
+
+    private BufferedReader print() {
         System.out.print("계산할 값을 입력해주세요(0만 입력시 종료) : ");
         return bufferedReader;
     }
+
     private void output(String answer) {
         System.out.println(answer);
     }
+
     public String getAnswer() {
         return answer;
     }
