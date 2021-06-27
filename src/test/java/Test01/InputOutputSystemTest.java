210b0fc3235b4beb6cba1720d2b12f06df755984
jeeseungbae <https://github.com/jeeseungbae/TIMD_project.git>
2021-06-27 14:56:31 +0900

    test : InputOutputSystemTest 계산 테스트

--- src/test/java/Test01/InputOutputSystemTest.java
+++ src/test/java/Test01/InputOutputSystemTest.java
@@ -25,4 +25,79 @@ public class InputOutputSystemTest {
             e.printStackTrace();
         }
     }
+    @DisplayName("정상적인 뺄셈으로 계산하여 출력한다.")
+    @Test
+    public void test2() {
+        String data = "10-2-2";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        InputOutputSystem inputOutputSystem = new InputOutputSystem();
+        try {
+            inputOutputSystem.question();
+            inputOutputSystem.compute();
+            double answer = Double.parseDouble(inputOutputSystem.getAnswer());
+            Assertions.assertEquals(answer, 6);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @DisplayName("정상적인 곱셈으로 계산하여 출력한다.")
+    @Test
+    public void test3() {
+        String data = "10*2*2";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        InputOutputSystem inputOutputSystem = new InputOutputSystem();
+        try {
+            inputOutputSystem.question();
+            inputOutputSystem.compute();
+            double answer = Double.parseDouble(inputOutputSystem.getAnswer());
+            Assertions.assertEquals(answer, 40);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @DisplayName("정상적인 나눗셈으로 계산하여 출력한다.")
+    @Test
+    public void test4() {
+        String data = "10/2/2";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        InputOutputSystem inputOutputSystem = new InputOutputSystem();
+        try {
+            inputOutputSystem.question();
+            inputOutputSystem.compute();
+            double answer = Double.parseDouble(inputOutputSystem.getAnswer());
+            Assertions.assertEquals(answer, 2.5);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @DisplayName("정상적인 여러가지 연산을 계산한다.")
+    @Test
+    public void test5() {
+        String data = "10+2-2/2*6+5";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        InputOutputSystem inputOutputSystem = new InputOutputSystem();
+        try {
+            inputOutputSystem.question();
+            inputOutputSystem.compute();
+            double answer = Double.parseDouble(inputOutputSystem.getAnswer());
+            Assertions.assertEquals(answer, 35);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @DisplayName("0을 입력시 0을 반환한다.")
+    @Test
+    public void test6() {
+        String data = "0";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        InputOutputSystem inputOutputSystem = new InputOutputSystem();
+        try {
+            inputOutputSystem.question();
+            inputOutputSystem.compute();
+            double answer = Double.parseDouble(inputOutputSystem.getAnswer());
+            Assertions.assertEquals(answer, 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
